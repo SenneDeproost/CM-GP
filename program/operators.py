98fadd3c3464dcfd644cb494c6049167bf629097
Senne Deproost <senne.deproost@vub.be>
2024-11-28 21:10:49 +0100

    Simple operators list updated

--- program/operators.py
+++ program/operators.py
@@ -8,26 +8,51 @@ from typing import List, Callable, Union
 import math
 import numpy as np
 
+
 # Simple operator for Python function
 class Operator:
-    def __init__(self, name: str, n_operands: int, function: Callable) -> None:
+    def __init__(self, name: str, n_operands: int, function: Callable, print=Callable) -> None:
         self.name = name
         self.n_operands = n_operands
         self.function = function
+        self.print = print
 
     # Dunder for operator name
     def __str__(self) -> str:
         return self.name
 
 SIMPLE_OPERATORS = [
-    Operator(name='+', n_operands=2, function=lambda a, b: a + b),
+    Operator('+', 2, lambda a, b: a + b,
+             lambda a, b: f'{a} + {b}'),
+    Operator('-', 2, lambda a, b: a - b,
+             lambda a, b: f'{a} - {b}'),
+    Operator('%', 2, lambda a, b: a % b,
+             lambda a, b: f'{a} % {b}'),
+    Operator('*', 2, lambda a, b: a * b,
+             lambda a, b: f'{a} * {b}'),
+    Operator('/', 2, lambda a, b: 999 if -0.01 > b > 0.01 else a / b,
+             lambda a, b: f'{a} / {b}'),
+    Operator('||', 1, lambda a: abs(a),
+             lambda a: f'|{a}|'),
+    Operator('exp', 1, lambda a: math.exp(a),
+             lambda a: f'exp({a})'),
+    Operator('sin', 1, lambda a: math.sin(a),
+             lambda a: f'sin({a})'),
+    Operator('cos', 1, lambda a: math.cos(a),
+             lambda a: f'cos({a})'),
+    Operator('log', 1, lambda a: -999 if a < 0.01 else math.log(a),
+             lambda a: f'log({a})'),
+    Operator('neg', 1, lambda a: -a,
+             lambda a: f'neg({a})'),
+    Operator('id', 1, lambda a: a,
+             lambda a: f'id({a})'),
+    Operator('>', 4, lambda a, b, _true, _false: _true if a > b else _false,
+             lambda a, b, _true, _false: f'{_true} if {a} > {b} else {_false}'),
+    Operator('<', 4, lambda a, b, _true, _false: _true if a < b else _false,
+             lambda a, b, _true, _false: f'{_true} if {a} < {b} else {_false}'),
 ]
 
-GONIOMETRIC_OPERATORS = [
-    Operator(name='sin', n_operands=1, function=lambda a: math.sin(a)),
-]
-
-OPERATOR_SETS = [SIMPLE_OPERATORS, GONIOMETRIC_OPERATORS]
+OPERATOR_SETS = [SIMPLE_OPERATORS]
 
 if __name__ == '__main__':
     for operator_set in OPERATOR_SETS:
